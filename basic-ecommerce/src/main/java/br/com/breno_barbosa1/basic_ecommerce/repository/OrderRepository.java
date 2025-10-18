aa3619e6cba98aef13dc02754df771835f521101
Breno-Barbosa1 <brenobarbosa117@gmail.com>
2025-10-18 08:07:06 -0300

    fixed a bug with the find by id method of the orderService

--- basic-ecommerce/src/main/java/br/com/breno_barbosa1/basic_ecommerce/repository/OrderRepository.java
+++ basic-ecommerce/src/main/java/br/com/breno_barbosa1/basic_ecommerce/repository/OrderRepository.java
@@ -3,12 +3,17 @@ package br.com.breno_barbosa1.basic_ecommerce.repository;
 import br.com.breno_barbosa1.basic_ecommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // loads all fields correctly
     @Query("SELECT o FROM Order o JOIN FETCH o.user u JOIN FETCH o.items item JOIN FETCH item.product")
     List<Order> findAllOrdersWithDetails();
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.user u JOIN FETCH o.items item JOIN FETCH item.product WHERE o.id = :orderId")
+    Optional<Order> findOrderByIdWithDetails(@Param("orderId") Long orderId);
 }
